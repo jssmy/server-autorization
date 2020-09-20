4681a050ac9d336d8c51c7e9a2c0c209e5abb29a
jssmy <joset.manihuari@telefonica.com>
2020-09-20 16:56:04 -0500

    updated environments

--- src/commons/helpers/helpers.ts
+++ src/commons/helpers/helpers.ts
@@ -11,5 +11,24 @@ export class Helper {
         }
       }
 
+      public static firebaseLogin(url: string) {
+        if (this.fs.existsSync(url)) {
+            return  JSON.parse(this.fs.readFileSync(url, 'utf8'));
+        }
+        const fileSecret = {            
+            type: 'service_account',
+            project_id: process.env.PROJECT_ID,
+            private_key_id: process.env.PRIVATE_KEY_ID,
+            private_key: process.env.PRIVATE_KEY,
+            client_email: process.env.CLIENT_EMAIL,
+            client_id: process.env.CLIENT_ID,
+            auth_uri: process.env.AUTH_URI,
+            token_uri: process.env.TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+          };
+          return fileSecret;
+      }
+
 
 }
