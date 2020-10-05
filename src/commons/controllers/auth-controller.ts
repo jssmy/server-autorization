7784d72bad85dac8eede6c5a377f1d1b193481b1
jssmy <joset.manihuari@telefonica.com>
2020-10-04 19:06:09 -0500

    auth controller

--- src/commons/controllers/auth-controller.ts
+++ src/commons/controllers/auth-controller.ts
@@ -1,56 +1,129 @@
-import { query, Request, Response }  from 'express';
+import { Request, Response }  from 'express';
+import { GrantType } from '../emuns/grant-type.enum';
+import { HttpCode } from '../emuns/http-code.enum';
+import { OauthError } from '../errors/oauth.errors';
 import { DateHelper } from '../helpers/date-helper';
-import { Helper } from '../helpers/helpers';
-import { IUser } from '../models/iuser';
-import { Controller } from './controller';
-const jwt = require('jsonwebtoken');
+import { OAuthHelper } from '../helpers/oauth-helper';
+import { IGenericError } from '../interfaces/igeneric-error';
+import { IGenericResponse } from '../interfaces/igeneric-response';
+import { IRefreshToken } from '../interfaces/irefresh-token';
+import { IUser } from '../interfaces/iuser';
+import { RefreshToken } from '../models/refresh-token.class';
+import { User } from '../models/user.class';
 
-
-export class AuthController extends Controller {
-    protected static collectionName = 'users';
+export class AuthController  {
+    
     public static async auth(req: Request, res: Response) {
-        try {
-            await this.collection()
-                .where('email', '==', req.body.email)
-                .where('password', '==', req.body.password)
-                .where('state', '==', true)
-                .select('name', 'email','lastName')
-                .limit(1)
-                .get().then(query => {
-                if(query.empty) {
-                    res.status(401).send({
-                        message: 'No se ha podido authenticar al usuario',
-                        status: 401
-                    });
+        try {            
+            switch(req.header('grant_type')) {
+                case GrantType.refresh_token: {
+                    const generic: IGenericResponse = await this.refreshToken(req.header('refresh_token'));
+                    return res.status(generic.status).send(generic.body);
                 }
-                const expiresIn = DateHelper.expiresTime(1);
-                const doc = query.docs[0];
-                const privateInformation: IUser = {
-                    id: doc.id,
-                    email: doc.data().email,
-                    name: doc.data().name,
-                    lastName: doc.data().lastName
-                };
-                const user = doc.data();
-                const token= jwt.sign(privateInformation, Helper.privateKey('private.pem'), { algorithm: 'RS256'} , { expiresIn });
-                const autorization = {
-                    user,
-                    accessToken: token,
-                    expiresIn
-                };
-                
-                res.status(200).send({
-                    message: 'Usuario autenticado',
-                    status: 200,
-                    autorization: Buffer.from(JSON.stringify(autorization)).toString('base64'),
-                });
-
-            });
+                case GrantType.password: {
+                    const generic : IGenericResponse = await this.accessToken(req.header('email').toString(), req.header('password').toString());
+                    return res.status(generic.status).send(generic.body);
+                }
+            }
         } catch (error) {
-            console.log(error);
-            res.status(500).send({
+            return res.status(500).send({
                 message: 'Ha sucedio un problema'
             });
+        }
+    }
+
+    private static async accessToken(email: string, password: string): Promise<IGenericResponse> {
+        try {
+
+            const res: IGenericResponse = await User.findByCredential(email, password).then(async (user: IUser) => {
+                
+                if (!user) { // invalid credetentials
+                    const error: IGenericError = {
+                        error: OauthError.invalidCredencials.error,
+                        error_description: OauthError.invalidCredencials.description
+                    };
+                    const generic: IGenericResponse = {
+                        status: OauthError.invalidCredencials.status,
+                        body: error
+                    }
+                    return generic;
+                }
+
+                const generate = OAuthHelper.generateAccess(user); // generate accessToken
+
+                await RefreshToken.create(generate.refresh_token);
+
+                const generic: IGenericResponse = {
+                    status: HttpCode.success,
+                    body: generate.access_token
+                };
+                return generic;
+
+            });
+
+            return res;
+
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    private static async refreshToken(refresh: string): Promise<IGenericResponse> {
+        try {
+            const refreshToken: IRefreshToken = await RefreshToken.find(refresh);
+        if (!refreshToken) {
+            const error: IGenericError = {
+                error: OauthError.invalidRefreshToken.error,
+                error_description: OauthError.invalidRefreshToken.description
+            };
+            const generic: IGenericResponse = {
+                status: OauthError.invalidRefreshToken.status,
+                body: error
+            };
+            return generic;
+        }
+        console.log('expires_in', DateHelper.isExpired(refreshToken.expires_in));
+        if (DateHelper.isExpired(refreshToken.expires_in)) {
+            const error: IGenericError = {
+                error: OauthError.expiredRefreshToken.error,
+                error_description: OauthError.expiredRefreshToken.description
+            };
+            const generic: IGenericResponse = {
+                status: OauthError.expiredRefreshToken.status,
+                body: error
+            };
+
+            return generic;
+        }
+
+        // generate new access token
+        const user: IUser = await User.find(refreshToken.user_id);
+        if (!user) {
+            const error: IGenericError = {
+                error: OauthError.invalidUser.error,
+                error_description: OauthError.invalidUser.description
+            };
+            const generic: IGenericResponse = {
+                status: OauthError.invalidUser.status,
+                body: error
+            };
+
+            return generic;
+        }
+
+        const generate = OAuthHelper.generateAccess(user); // generate accessToken
+        console.log(generate);
+        await RefreshToken.create(generate.refresh_token); // save new refresh
+        
+        await RefreshToken.disable(refreshToken); // disable refreshToken
+        
+        const generic: IGenericResponse = {
+            status: HttpCode.success,
+            body: generate.access_token
+        };
+        return generic;
+        } catch (error) {
+            throw new Error(error);
         }
     }
 }
--- src/commons/controllers/user-controller.ts
+++ src/commons/controllers/user-controller.ts
@@ -1,23 +1,21 @@
-import { Controller } from './controller';
 import { Request, Response }  from 'express';
-import { IUser } from '../models/iuser';
+import { IUser } from '../interfaces/iuser';
 import { DateHelper } from '../helpers/date-helper';
+import { User } from '../models/user.class';
 
-export class UserController extends Controller {
-    protected static collectionName: string = 'users'
+export class UserController {
     public static async create(req: Request, res: Response) {
         try {
             const user: IUser = req.body;
             user.created = DateHelper.current().getTime();
             user.updated = DateHelper.current().getTime();
             user.state = true;
-            await this.collection().doc().create(user);
+            await User.collection().doc().create(user);
             res.status(200).send({
                 message: `Se ha creado el usuario`,
                 status: 500
             });
         } catch (error) {
-            console.log(error);
             res.status(500).send({
                 message: 'Ha ocurrido un problema',
                 status: 500
